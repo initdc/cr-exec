fdb256b152648cecc04621acc5243eb604191456
initdc <initd@outlook.com>
2023-12-29 15:08:06 +0000

    chore: add code base

---
+++ lib/cr/exec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "exec/version"
+
+module Cr
+  module Exec
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cr/exec/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Cr
+  module Exec
+    VERSION = "0.1.0"
+  end
+end
